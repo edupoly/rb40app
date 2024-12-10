6f76352a9602fb23541c2acd76a1435cd6a3abd7
edupoly <info.edupoly@gmail.com>
2024-12-10 08:16:11 +0530

    zcx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Edupoly</h1>
+      <button className="btn btn-primary">CLICK</button>
+      <button className="btn btn-secondary">CLICK</button>
+      <button className="btn btn-danger">CLICK</button>
+      <button className="btn btn-success">CLICK</button>
+      <button className="btn btn-info">CLICK</button>
+      <button className="btn btn-dark">CLICK</button>
+      <button className="btn btn-light">CLICK</button>
     </div>
   );
 }
