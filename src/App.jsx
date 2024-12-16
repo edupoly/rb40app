bf781ee1c84d27affb2a461f61e61a655b644545
edupoly <info.edupoly@gmail.com>
2024-12-16 08:45:28 +0530

    callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-
+import { useState } from "react";
 function App() {
+  var [x,setX] = useState(1);
+  function inc(){
+    setX((pv)=>{return pv+1})
+  }
+  function inc10(){
+
+    setTimeout(()=>{
+      setX((pv)=>{return pv+1})
+    },5000)
+
+    setX(x+5)
+
+  }
   return (
-    <div className="App">
-      <h1>Welcome to Edupoly</h1>
+    <div className="m-4">
+      <h1>Count:{x}</h1>
+      <button onClick={()=>{inc()}}>Increment</button>
+      <button onClick={()=>{inc10()}}>Increment10</button>
     </div>
   );
 }
