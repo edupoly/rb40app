2a9cbae38c70d8a3e2cae1948e26da2a9dee3a17
edupoly <info.edupoly@gmail.com>
2024-12-16 18:25:08 +0530

    fetchcalls

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  var [products,setProducts]=useState([]);
+  
+  useEffect(()=>{
+    const url = 'https://pinnacle-odds.p.rapidapi.com/kit/v1/special-markets?is_have_odds=true&sport_id=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '9e8e4f62b2msh562197ec14c334ep17b241jsn765d128bb53d',
+        'x-rapidapi-host': 'pinnacle-odds.p.rapidapi.com'
+      }
+    };
+    
+    fetch(url,options)
+    .then(res=>res.json())
+    .then((data)=>{
+      console.log(data)
+      setProducts([...data.specials])
+    })
+    
+  },[])
+
   return (
     <div className="App">
       <h1>Welcome to Edupoly</h1>
+      <ul>
+        {
+          products?.map((product)=>{
+            return <li>{product.name}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
