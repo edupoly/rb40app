f345d725ff76c6c0e280c9df6758177ad6142252
edupoly <info.edupoly@gmail.com>
2025-01-06 08:40:56 +0530

    sdfsd

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Todo from './Todo';
 
 function Todolist(){
     var [a,setA]=React.useState(13)
+    var [newtodo,setNewTodo] = React.useState('')
     var [todos,setTodos]=React.useState(['get car','pay rent','clear bills','clean bike'])
     function addTodo(){
-        var s = document.getElementById("d1").value;
-        setTodos([...todos,s])
+        setTodos([...todos,newtodo])
     }
+
     function deleteTodo(ind){
         var temp = [...todos]
         temp.splice(ind,1)
         setTodos([...temp])
     }
-    
-    React.useEffect(()=>{console.log("Deepika")},[]);
-    React.useEffect(()=>{console.log("Drithiman")})
-    React.useEffect(()=>{console.log("Venkatesh")},[a])
+    useEffect(()=>{console.log("Todolist re-renderd")})
     return (
         <div className="p-2 border border-5 m-3 border-danger">
-            {console.log("Inside UI")}
-            <input type="text" id='d1'/>
+            <input type="text" id='d1' onChange={(e)=>{setNewTodo(e.target.value)}}/>
             <button onClick={()=>{addTodo()}}>Add Todo</button>
             <ul>
                 {
