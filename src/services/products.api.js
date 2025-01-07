03b31f917297c112a2af8da3160a3ef0c4b5c85c
edupoly <info.edupoly@gmail.com>
2025-01-07 09:20:35 +0530

    xcv

--- src/services/products.api.js
+++ src/services/products.api.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com/products' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/products' }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => `/`,
