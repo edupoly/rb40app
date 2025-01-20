80bcaa4564db291e387b1580ea0c27b8e74ff857
edupoly <info.edupoly@gmail.com>
2025-01-20 10:55:07 +0530

    passingprops

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Autocomplete, Button,TextField } from "@mui/material";
 
 function App() {
+  const options = [
+    { label: 'The Godfather', id: 1 },
+    { label: 'Pulp Fiction', id: 2 },
+  ];
   return (
     <div className="App">
       <h1>Welcome to Edupoly</h1>
+      <Button variant="contained">Welcome to Material UI Button</Button>
+      <Autocomplete
+        disablePortal
+        options={options}
+        sx={{ width: 900 }}
+
+        renderInput={(params) => <TextField {...params} label="Movie" />}
+      />
     </div>
   );
 }
