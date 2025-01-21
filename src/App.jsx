8181f8fe113405895cdc56dc2a2127441faef293
edupoly <info.edupoly@gmail.com>
2025-01-21 10:46:42 +0530

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>ReactJS Training in Hyderabad at Edupoly</h1>
-      <h2>useMemo based performance imporovement</h2>
+      <h2>useMemo based performance improvement</h2>
       <EmployeeList></EmployeeList>
     </div>
   );
